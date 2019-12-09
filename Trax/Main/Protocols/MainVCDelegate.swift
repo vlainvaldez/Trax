b2f3aef6d5373ea7927476743484afa922032034
alvin joseph valdez <alvinjosephvaldez@alvins-MacBook-Pro.local>
2019-12-09 23:11:58 +0800

    Initial Commit with actual codes

---
+++ Trax/Main/Protocols/MainVCDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  MainVCDelegate.swift
+//  Trax
+//
+//  Created by alvin joseph valdez on 12/9/19.
+//  Copyright © 2019 alvin joseph valdez. All rights reserved.
+//
+
+import Foundation
+
+public protocol MainVCDelegate: class {
+    
+}
---
+++ Trax/MainVC.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Trax
+//
+//  Created by alvin joseph valdez on 12/9/19.
+//  Copyright © 2019 alvin joseph valdez. All rights reserved.
+//
+
+import UIKit
+
+public final class MainVC: UIViewController {
+    
+    // MARK: Delegate Declarations
+    public weak var delegate: MainVCDelegate?
+    
+    // MARK: - Initializer
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    public required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Deinitializer
+    deinit {
+        print("\(type(of: self)) was deallocated")
+    }
+    
+    // MARK: - LifeCycle Methods
+    public override func loadView() {
+        super.loadView()
+        self.view = MainView()
+    }
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Trax"
+    }
+}
+
+// MARK: - Views
+extension MainVC {
+    public var rootView: MainView { return self.view as! MainView }
+}
+
